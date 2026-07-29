77dd7e9d778e8c2f3bc3b68496f734815f3c342b
embedded-sergey <embedded.sergey@gmail.com>
2026-07-29 06:03:58 +0300

    Add static compensation temp, rename trigger step, improve RTD validation, clean LCD output

--- firmware/SensResp/SensResp.ino
+++ firmware/SensResp/SensResp.ino
@@ -1,11 +1,15 @@
-// Include Arduino Wire and Atlas Scientific libraries for I2C protocol
+// Include following libraries to enable I2C protocol
 #include <Wire.h>  // include the standard I2C library
 #include <Ezo_i2c.h>  // include the EZO I2C library from https://github.com/Atlas-Scientific/Ezo_I2c_lib
-#include <sequencer4.h>  // imports a 2 function sequencer 
+#include <sequencer4.h>  // imports a 2 function sequencer
 #include <Ezo_i2c_util.h>  // brings in common print statements
-#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_I2C.h>  // I2C for LCD 20x4 display
 
-//define I2C addresses for all the sensors and devices except SensResp 
+// Temperature used for DO compensation when RTD reading is invalid (safe to edit by a user)
+const float static_compensation_temperature = 20.0; // if no actual or valid value from RTD received
+const float invalid_temperature = -273.15; // RTD readings below this value are considered invalid
+
+//define I2C addresses for all the sensors and devices except SensResp
 #define PumpResp_address 99  // define I2C address for PumpResp
 #define PumpResp_size 5  // define the maximum size of 'Phase' string from PumpResp
 int LED = 4;
@@ -19,24 +23,24 @@ Ezo_board RTD = Ezo_board(100, "");  // create an RTD circuit object which addre
 Ezo_board DO_1 = Ezo_board(101, "");
 Ezo_board DO_2 = Ezo_board(102, "");
 Ezo_board DO_3 = Ezo_board(103, "");
-Ezo_board DO_4 = Ezo_board(104, ""); 
+Ezo_board DO_4 = Ezo_board(104, "");
 
 // Forward declarations of functions to use them in the sequencer before defining them
-void step1();  
-void step2();
-void step3();
-void step4();
+void triggerPumpRespAndRTD();
+void sendTemperatureToDO();
+void requestDOReadings();
+void collectAllData();
 
 // Calls the steps in sequence with time in between them
-Sequencer4 Seq(&step1, 50,
-               &step2, 300,
-               &step3, 600,
-               &step4, 30);
+Sequencer4 Seq(&triggerPumpRespAndRTD, 50,
+               &sendTemperatureToDO, 300,
+               &requestDOReadings, 600,
+               &collectAllData, 30);
 
 void setup(){
   Serial.begin(9600);  // start the serial communication to PC with default baud rate
   Serial.println("CLEARRANGE,A,2,H,1000000"); // trick to get rid of AM-PM
-  pinMode(LED, OUTPUT); 
+  pinMode(LED, OUTPUT);
   digitalWrite(LED, HIGH);
   
   Wire.begin();  // start the I2C without an address inside as SensResp is a master device
@@ -47,78 +51,70 @@ void setup(){
   LCD.clear();
 }
 
+
 void loop() {
   Seq.run();  // run the sequencer to do the polling
 }
 
-void step1() {
+
+void triggerPumpRespAndRTD() {
   // Write a start signal (i.e. zero) to the PumpResp
   Wire.beginTransmission(PumpResp_address);
   Wire.write(0);
   Wire.endTransmission();
-  
-  //DO_1.send_cmd("S, 31, ppt");
-  // Send a command to read data from sensors  
   
   //send a read command. we use this command instead of RTD.send_cmd("R"); 
   //to let the library know to parse the reading
   RTD.send_read_cmd();
 }
 
-void step2() {
-     
+
+void sendTemperatureToDO() {
   // If the temperature reading has been received and it is valid
-  if ((RTD.get_error() == Ezo_board::SUCCESS) && (RTD.get_last_received_reading() > -1000.0)) {  
-     DO_1.send_cmd_with_num("T,", RTD.get_last_received_reading());
-     DO_2.send_cmd_with_num("T,", RTD.get_last_received_reading());
-     DO_3.send_cmd_with_num("T,", RTD.get_last_received_reading());
-     DO_4.send_cmd_with_num("T,", RTD.get_last_received_reading());
+  if ((RTD.get_error() == Ezo_board::SUCCESS) && (RTD.get_last_received_reading() > invalid_temperature)) {  
+    DO_1.send_cmd_with_num("T,", RTD.get_last_received_reading());
+    DO_2.send_cmd_with_num("T,", RTD.get_last_received_reading());
+    DO_3.send_cmd_with_num("T,", RTD.get_last_received_reading());
+    DO_4.send_cmd_with_num("T,", RTD.get_last_received_reading());
   } else {                
-    DO_1.send_cmd_with_num("T,", 10.0);   
-    DO_2.send_cmd_with_num("T,", 10.0); 
-    DO_3.send_cmd_with_num("T,", 10.0); 
-    DO_4.send_cmd_with_num("T,", 10.0);  
+    DO_1.send_cmd_with_num("T,", static_compensation_temperature);   
+    DO_2.send_cmd_with_num("T,", static_compensation_temperature); 
+    DO_3.send_cmd_with_num("T,", static_compensation_temperature); 
+    DO_4.send_cmd_with_num("T,", static_compensation_temperature);  
   }
 }
 
-void step3() {
-  // Send the read command to DO circuits
+
+void requestDOReadings() {
   DO_1.send_read_cmd(); 
   DO_2.send_read_cmd();            
   DO_3.send_read_cmd();            
   DO_4.send_read_cmd();            
 }
 
-void step4(){
+
+void collectAllData(){
   // Request data and read response from PumpResp
   Wire.requestFrom(PumpResp_address, PumpResp_size); 
   phase = ""; // add characters to a string (i.e. phase)
    while (Wire.available()) {
     char b = Wire.read();
-    
     // Ignore common non-printable or trailing padding bytes
     if (b == '\0' || b == '\r' || b == '\n') {
       break; // Stop reading if the slave sent a terminator
     }
-    
     phase += b;
   } 
   
   // Explicitly trim any unexpected accidental whitespace
   phase.trim();
   Serial.print(phase);
-  Serial.print("\t");
-
   // Get the reading from the RTD circuit
-  receive_and_print_reading(RTD);  // get the reading from the RTD circuit and print it
-  Serial.print("\t");
+  receive_and_print_reading(RTD);
   // Get the reading from the DO circuits
   receive_and_print_reading(DO_1);
-  Serial.print("\t");
   receive_and_print_reading(DO_2);
-  Serial.print("\t");
   receive_and_print_reading(DO_3);
-  Serial.print("\t");
   receive_and_print_reading(DO_4);
   Serial.println();
 
@@ -135,37 +131,19 @@ void step4(){
 
 
 void LCD_code() {
+    // Dissolved Oxygen readings
+    LCD.setCursor(0, 0); LCD.print("DO1: " + String(DO_1.get_last_received_reading()) + "   ");
+    LCD.setCursor(0, 1); LCD.print("DO2: " + String(DO_2.get_last_received_reading()) + "   ");
+    LCD.setCursor(0, 2); LCD.print("DO3: " + String(DO_3.get_last_received_reading()) + "   ");
+    LCD.setCursor(0, 3); LCD.print("DO4: " + String(DO_4.get_last_received_reading()) + "   ");
 
-    // --- DO readings ---
-    LCD.setCursor(0, 0);
-    LCD.print("DO1: ");
-    LCD.print(String(DO_1.get_last_received_reading()) + "   ");
+    // PumpResp phase: F - Flush, M - Measurement
+    LCD.setCursor(13, 0); LCD.print("Phase:");
+    LCD.setCursor(13, 1); LCD.print(phase + "  ");
 
-    LCD.setCursor(0, 1);
-    LCD.print("DO2: ");
-    LCD.print(String(DO_2.get_last_received_reading()) + "   ");
-
-    LCD.setCursor(0, 2);
-    LCD.print("DO3: ");
-    LCD.print(String(DO_3.get_last_received_reading()) + "   ");
-
-    LCD.setCursor(0, 3);
-    LCD.print("DO4: ");
-    LCD.print(String(DO_4.get_last_received_reading()) + "   ");
-
-    // --- Phase ---
-    LCD.setCursor(13, 0);
-    LCD.print("Phase:");
-
-    LCD.setCursor(13, 1);
-    LCD.print(phase + "   ");  // pad to clear leftovers
-
-    // --- Temperature ---
-    LCD.setCursor(13, 2);
-    LCD.print("Temp:");
-
-    LCD.setCursor(13, 3);
-    LCD.print(String(RTD.get_last_received_reading()) + "  ");
+    // Temperature readings
+    LCD.setCursor(13, 2); LCD.print("Temp:");
+    LCD.setCursor(13, 3); LCD.print(String(RTD.get_last_received_reading()) + "  ");
 }
 
 
